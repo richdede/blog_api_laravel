be7a6f65ad168a706407d9e3fb73111c3a5fe92f
Dee Prince <richdede>
2024-05-02 04:31:40 -0700

    register and login api

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegistrationRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Login method
+     */
+
+     public function login(LoginRequest $request)
+     {
+        $token = auth()->attempt($request->validated());
+        if ($token) {
+            return $this->responseWithToken($token, auth()->user());
+        } else {
+            return response()->json(
+                [
+                    'status' => 'failed',
+                    'message' => 'Invalid Credentials',
+                ],
+                401
+            );
+        }
+     }
+
+     /**
+      * Registration method
+      */
+
+      public function register(RegistrationRequest $request)
+      {
+        $user = User::create($request->validated());
+        if ($user) {
+            $token = auth()->login($user);
+            return $this->responseWithToken($token, $user);
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'An error occurred while creating user',
+            ], 500);
+        }
+
+      }
+
+          /**
+     * Return jwt access token
+     */
+
+    public function responseWithToken($token, $user)
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+        ]);
+    }
+}
